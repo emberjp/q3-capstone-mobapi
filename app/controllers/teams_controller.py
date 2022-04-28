34c67da72197eb36f8e9474d12dd9c63cdc2d5ca
Pablo Rocha Goncalves <pablorgoncalves@gmail.com>
2022-04-28 16:23:07 -0300

    feat: teams_controller get_teams

--- app/controllers/teams_controller.py
+++ app/controllers/teams_controller.py
@@ -1,4 +1,6 @@
+from flask import jsonify
 from app.configs.database import db
+from app.models import Team
 from sqlalchemy import exc
 from sqlalchemy.orm import Query
 from sqlalchemy.orm.session import Session
@@ -9,7 +11,10 @@ def add_team():
 
 
 def get_teams():
-    ...
+    base_query: Query = db.session.query(Team)
+    teams = base_query.order_by(Team.id).all()
+
+    return jsonify(teams), 200
 
 
 def get_team(id):
